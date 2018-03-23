dab8e4af4ca6c2d4f31eb24606060d60ab3354be
root <root@deb01.localdomain>
2018-03-23 17:50:58 +0100

    add post request support

--- randstr.py
+++ randstr.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+def genStr (n):
 	str = ''
 	i = 0
-	while i <= 2048:
+	while i < n:
 		c = chr (random.randint(33,126))
 		if c.isalpha():
 			str += c
 			i += 1
 	return str
+
+@app.route("/")
+def main_get():
+	return genStr(2048)
+
+@app.route("/",methods=['POST'])
+def main_post():
+	try:
+		n = int(request.form['n'])
+		return genStr(n)
+	except Exception:
+		return "ERROR"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
